5192f69ed5c2ee5109de61e5ddeeb9808ea1a479
Ajayj007 <ajayjadhav7.skn.comp@gmail.com>
2026-01-26 15:36:22 +0530

    remaining files uploaded

---
+++ farm_Backend/src/test/java/com/pm/farm_backend/FarmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.farm_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
