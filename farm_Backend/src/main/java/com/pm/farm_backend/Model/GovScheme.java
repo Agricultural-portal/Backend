f5e9309759e83176b4bde43fa69c09ce22d85701
Omkarpatil6168 <omkar.p4905@gmail.com>
2026-01-26 15:18:39 +0530

    added GovScheme Entity

---
+++ farm_Backend/src/main/java/com/pm/farm_backend/Model/GovScheme.java
@@ -0,0 +1,24 @@
+package com.pm.farm_backend.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "gov_schemes")
+public class GovScheme {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String schemeName;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+}
