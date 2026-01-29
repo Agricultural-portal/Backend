7df607070969d555ed31fe7acbc581b2ceca0244
Omkarpatil6168 <omkar.p4905@gmail.com>
2026-01-29 12:43:41 +0530

    Goverment scheme functionality added for farmer

--- src/main/java/com/pm/farm_backend/Controller/FarmerController/GovSchemeController.java
+++ src/main/java/com/pm/farm_backend/Controller/FarmerController/GovSchemeController.java
@@ -1,7 +1,7 @@
 package com.pm.farm_backend.Controller.FarmerController;
 
 import com.pm.farm_backend.Model.GovScheme;
-import com.pm.farm_backend.Service.GovSchemeService;
+import com.pm.farm_backend.Service.FarmerService.GovSchemeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,19 +10,19 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/gov-schemes")
-@CrossOrigin(origins = "*")
+@org.springframework.security.access.prepost.PreAuthorize("hasRole('FARMER')")
 public class GovSchemeController {
 
     @Autowired
     private GovSchemeService govSchemeService;
 
     @GetMapping("/all")
-    public ResponseEntity<List<GovScheme>> getAllSchemes() {
+    public ResponseEntity<List<GovScheme>> getAllSchemes(java.security.Principal principal) {
         return ResponseEntity.ok(govSchemeService.getAllSchemes());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<GovScheme> getSchemeById(@PathVariable Long id) {
+    public ResponseEntity<GovScheme> getSchemeById(@PathVariable Long id, java.security.Principal principal) {
         return govSchemeService.getSchemeById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
